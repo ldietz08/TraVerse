3671ec6a9b270da9346988896c05d9ff6038a575
Lakpa Dietz <ldietz086@gmail.com>
2023-02-08 09:51:14 -0800

    firebase config added to env

--- client/src/firebase/Firebase.js
+++ client/src/firebase/Firebase.js
@@ -15,7 +15,7 @@ import { useEffect } from "react";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBHqtyO7YFNa0ht4JUbX9N32lKyNuS2aH4",
+  apiKey: `${process.env.apiKey}`,
   authDomain: `${process.env.authDomain}`,
   projectId: `${process.env.projectId}`,
   storageBucket: `${process.env.storageBucket}`,
